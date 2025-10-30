f899d141fb27f25ca281ab2e7be02872bf5fadb5
Maryna <maryny.khrystyianyn@gmail.com>
2025-10-30 22:51:28 +0100

    seed db

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,28 @@
-import Image from "next/image";
 
-export default function Home() {
+import { PrismaClient, User } from "@/app/generated/prisma/client";
+
+const prisma = new PrismaClient
+
+
+
+
+
+export default async function Home() {
+ 
+const users:User[] =  await prisma.user.findMany()
+if(!users) return <div>Loading...</div>
+
+console.log(users);
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <h1>Hello world</h1>
+    <div className=" text-amber-500">
+{
+  users.map((user)=>{
+    return(<div key={user.id}>
+{user.name}
+    </div>)
+  })
+}
+      
     </div>
   );
 }
---
+++ prisma/migrations/20251030201313_init/migration.sql
@@ -0,0 +1,46 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "img" TEXT,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Habit" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "userId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Habit_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "HabitLog" (
+    "id" SERIAL NOT NULL,
+    "habitId" INTEGER NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "completed" BOOLEAN NOT NULL DEFAULT true,
+    "note" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "HabitLog_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "HabitLog_habitId_date_key" ON "HabitLog"("habitId", "date");
+
+-- AddForeignKey
+ALTER TABLE "Habit" ADD CONSTRAINT "Habit_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "HabitLog" ADD CONSTRAINT "HabitLog_habitId_fkey" FOREIGN KEY ("habitId") REFERENCES "Habit"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
