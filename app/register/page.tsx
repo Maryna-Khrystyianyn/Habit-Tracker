e2f6c1072f7942f57061793eaefbaa1c0c787bbb
Maryna <maryny.khrystyianyn@gmail.com>
2025-10-31 13:02:21 +0100

    register

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,9 +1,53 @@
-import React from 'react'
+"use client";
 
-const pageRegister = () => {
+import React, { useState } from "react";
+
+const RegisterPage = () => {
+  const [form, setForm] = useState({ name: "", email: "", password: "" });
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const res = await fetch("/api/auth/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(form),
+    });
+    const data = await res.json();
+    setMessage(data.message || data.error);
+  };
+
   return (
-    <div>pageRegister</div>
-  )
-}
-
-export default pageRegister
+    <div className="flex flex-col text-[#819dfe]  items-center justify-center min-h-screen gap-4">
+      <h1 className="text-2xl font-bold">Register</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-2 w-64">
+        <input
+          type="text"
+          placeholder="Name"
+          value={form.name}
+          onChange={(e) => setForm({ ...form, name: e.target.value })}
+          className="border p-2 rounded focus:outline focus:outline-[#74a501] "
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          value={form.email}
+          onChange={(e) => setForm({ ...form, email: e.target.value })}
+          className="border p-2 rounded focus:outline focus:outline-[#74a501] "
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={form.password}
+          onChange={(e) => setForm({ ...form, password: e.target.value })}
+          className="border p-2 rounded focus:outline focus:outline-[#74a501] "
+        />
+        <button className="bg-blue-500 text-white p-2 rounded" type="submit">
+          Register
+        </button>
+      </form>
+      {message && <p>{message}</p>}
+    </div>
+  );
+};
+export default RegisterPage;
